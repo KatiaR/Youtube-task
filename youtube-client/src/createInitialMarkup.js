c2d9029d05b2120c6a8dcb022684c40ff71b8a25
Екатерина Романюк <katia-l@mail.ru>
2018-11-21 23:00:20 +0300

    refactor: move function from fetch

--- youtube-client/src/createInitialMarkup.js
+++ youtube-client/src/createInitialMarkup.js
@@ -1,56 +1,61 @@
-// import { addAttr } from './utils';
+import { setPageToken } from './getData';
+import { addAttr } from './utils';
 
-// export default class CreateMarkup {
-//   static createInitialMarkup(array) {
-//     const sliderHtml = document.getElementsByClassName('wrapper-slider')[0];
-//     array.forEach((elem) => {
-//       const { snippet } = elem;
-//       const dataContainer = document.createElement('div');
-//       dataContainer.id = elem.id.videoId;
-//       dataContainer.className = 'data-container';
-//       sliderHtml.appendChild(dataContainer);
+export default class CreateMarkup {
+  static createInitialMarkup(data) {
+    const array = data.items;
+    setPageToken(data.nextPageToken);
+    const listIds = array.map(item => item.id.videoId);
+    const sliderHtml = document.getElementsByClassName('wrapper-slider')[0];
+    array.forEach((elem) => {
+      const { snippet } = elem;
+      const dataContainer = document.createElement('div');
+      dataContainer.id = elem.id.videoId;
+      dataContainer.className = 'data-container';
+      sliderHtml.appendChild(dataContainer);
 
-//       const fragment = document.createDocumentFragment();
+      const fragment = document.createDocumentFragment();
 
-//       const divClip = document.createElement('div');
-//       divClip.className = 'clip';
-//       fragment.appendChild(divClip);
+      const divClip = document.createElement('div');
+      divClip.className = 'clip';
+      fragment.appendChild(divClip);
 
-//       const divAuthor = document.createElement('span');
-//       divAuthor.className = 'author';
-//       fragment.appendChild(divAuthor);
+      const divAuthor = document.createElement('span');
+      divAuthor.className = 'author';
+      fragment.appendChild(divAuthor);
 
-//       const divTitle = document.createElement('h2');
-//       fragment.appendChild(divTitle);
+      const divTitle = document.createElement('h2');
+      fragment.appendChild(divTitle);
 
-//       const divPublicationDate = document.createElement('span');
-//       divPublicationDate.className = 'publication-date';
-//       fragment.appendChild(divPublicationDate);
+      const divPublicationDate = document.createElement('span');
+      divPublicationDate.className = 'publication-date';
+      fragment.appendChild(divPublicationDate);
 
-//       const divDescription = document.createElement('p');
-//       divDescription.className = 'description';
-//       fragment.appendChild(divDescription);
+      const divDescription = document.createElement('p');
+      divDescription.className = 'description';
+      fragment.appendChild(divDescription);
 
-//       divTitle.textContent = snippet.title;
-//       divAuthor.textContent = `\uD83D\uDC68 ${snippet.channelTitle}`;
-//       divDescription.textContent = snippet.description;
+      divTitle.textContent = snippet.title;
+      divAuthor.textContent = `\uD83D\uDC68 ${snippet.channelTitle}`;
+      divDescription.textContent = snippet.description;
 
-//       const date = new Date(snippet.publishedAt);
-//       const twoNumberDate = number => (number < 10 ? `0${number}` : number);
-//       divPublicationDate.textContent = `\uD83D\uDCC5 ${date.getFullYear()}-${twoNumberDate(
-//         date.getMonth() + 1,
-//       )}-${twoNumberDate(date.getDate())}`;
+      const date = new Date(snippet.publishedAt);
+      const twoNumberDate = number => (number < 10 ? `0${number}` : number);
+      divPublicationDate.textContent = `\uD83D\uDCC5 ${date.getFullYear()}-${twoNumberDate(
+        date.getMonth() + 1,
+      )}-${twoNumberDate(date.getDate())}`;
 
-//       const img = document.createElement('img');
-//       const imgUrl = elem.snippet.thumbnails.high.url;
-//       const attrimg = {
-//         alt: 'youtube-video',
-//         src: imgUrl,
-//       };
-//       addAttr(img, attrimg);
-//       divClip.appendChild(img);
-//       fragment.appendChild(divTitle);
-//       dataContainer.appendChild(fragment);
-//     });
-//   }
-// }
+      const img = document.createElement('img');
+      const imgUrl = elem.snippet.thumbnails.high.url;
+      const attrimg = {
+        alt: 'youtube-video',
+        src: imgUrl,
+      };
+      addAttr(img, attrimg);
+      divClip.appendChild(img);
+      fragment.appendChild(divTitle);
+      dataContainer.appendChild(fragment);
+    });
+    return listIds;
+  }
+}
--- youtube-client/src/getData.js
+++ youtube-client/src/getData.js
@@ -1,9 +1,11 @@
-import { addAttr, numberLastPage } from './utils';
+import { numberLastPage } from './utils';
 import attHidden from './index';
-//import  CreateMarkup  from './createInitialMarkup.';
+import CreateMarkup  from './createInitialMarkup';
 
 
-export const slider = { position: 0, blocksPerPage: 1, amountContainer: 0, loading: false };
+export const slider = {
+  position: 0, blocksPerPage: 1, amountContainer: 0, loading: false,
+};
 
 const url = token => 'https://www.googleapis.com/youtube/v3/search'
 + `?key=AIzaSyC-hssxGiAeTHERVfsB2sEU5bowi0Lawhg&pageToken=${
@@ -20,6 +22,10 @@ export const resetPageToken = () => {
   pageToken = '';
 };
 
+export const setPageToken = (token) => {
+  pageToken = token;
+};
+
 export default function getDataFromYoutube() {
   const inputSearch = document.getElementById('search');
   if (inputSearch.value.length < 2) { return; }
@@ -27,59 +33,7 @@ export default function getDataFromYoutube() {
   fetch(`${url(pageToken)}${inputSearch.value}`)
     .then(response => response.json())
     .then((data) => {
-      const array = data.items;
-      pageToken = data.nextPageToken;
-      const listIds = array.map(item => item.id.videoId);
-      const sliderHtml = document.getElementsByClassName('wrapper-slider')[0];
-      array.forEach((elem) => {
-        const { snippet } = elem;
-        const dataContainer = document.createElement('div');
-        dataContainer.id = elem.id.videoId;
-        dataContainer.className = 'data-container';
-        sliderHtml.appendChild(dataContainer);
-
-        const fragment = document.createDocumentFragment();
-
-        const divClip = document.createElement('div');
-        divClip.className = 'clip';
-        fragment.appendChild(divClip);
-
-        const divAuthor = document.createElement('span');
-        divAuthor.className = 'author';
-        fragment.appendChild(divAuthor);
-
-        const divTitle = document.createElement('h2');
-        fragment.appendChild(divTitle);
-
-        const divPublicationDate = document.createElement('span');
-        divPublicationDate.className = 'publication-date';
-        fragment.appendChild(divPublicationDate);
-
-        const divDescription = document.createElement('p');
-        divDescription.className = 'description';
-        fragment.appendChild(divDescription);
-
-        divTitle.textContent = snippet.title;
-        divAuthor.textContent = `\uD83D\uDC68 ${snippet.channelTitle}`;
-        divDescription.textContent = snippet.description;
-
-        const date = new Date(snippet.publishedAt);
-        const twoNumberDate = number => (number < 10 ? `0${number}` : number);
-        divPublicationDate.textContent = `\uD83D\uDCC5 ${date.getFullYear()}-${twoNumberDate(
-          date.getMonth() + 1,
-        )}-${twoNumberDate(date.getDate())}`;
-
-        const img = document.createElement('img');
-        const imgUrl = elem.snippet.thumbnails.high.url;
-        const attrimg = {
-          alt: 'youtube-video',
-          src: imgUrl,
-        };
-        addAttr(img, attrimg);
-        divClip.appendChild(img);
-        fragment.appendChild(divTitle);
-        dataContainer.appendChild(fragment);
-      });
+      const listIds = CreateMarkup.createInitialMarkup(data);
       return fetch(urlView(listIds));
     })
     .then(response => response.json())
